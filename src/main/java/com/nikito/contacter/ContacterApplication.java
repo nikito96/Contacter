0adbfe421a87bea515fab2e560b9096b16c7a2e0
nikito96 <nikinikolitsov9655@gmail.com>
2019-02-04 01:34:16 +0200

    Init commit

---
+++ src/main/java/com/nikito/contacter/ContacterApplication.java
@@ -0,0 +1,14 @@
+package com.nikito.contacter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContacterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContacterApplication.class, args);
+	}
+
+}
+
