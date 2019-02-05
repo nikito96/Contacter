fa9dd442804463cb22ca127d683be90dc9ecd33f
nikito96 <nikinikolitsov9655@gmail.com>
2019-02-06 00:37:21 +0200

    Saving contacts works

--- src/main/java/com/nikito/contacter/ContactController.java
+++ src/main/java/com/nikito/contacter/ContactController.java
@@ -2,7 +2,10 @@ package com.nikito.contacter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
@@ -12,19 +15,14 @@ public class ContactController {
 	private ContactRepository contactRepository;
 	
 	@GetMapping("/addContact")
-	public String addContact() {
+	public String addContact(Model model) {
+		model.addAttribute("contact", new Contact());
 		return "addContact";
 	}
 	
-	@GetMapping("/add")
-	public String add(@RequestParam(name="name") String name, 
-			@RequestParam(name="number") String number, 
-			@RequestParam(name="address") String address) {
-		Contact c = new Contact();
-		c.setName(name);
-		c.setNumber(number);
-		c.setAddress(address);
-		contactRepository.save(c);
+	@PostMapping("/add")
+	public String add(@ModelAttribute Contact contact) {
+		contactRepository.save(contact);
 		return "addContact";
 	}
 }
