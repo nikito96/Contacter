d78bf6d9d92210526f0aa63116a397ebdea5c500
nikito96 <nikinikolitsov9655@gmail.com>
2019-02-10 02:04:03 +0200

    Some changes

--- src/main/java/com/nikito/contacter/Contact.java
+++ src/main/java/com/nikito/contacter/Contact.java
@@ -4,6 +4,8 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Contact {
@@ -12,10 +14,14 @@ public class Contact {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	
+	@NotNull
 	private String name;
 	
+	@NotNull
+	@Size(min=10, max=15)
 	private String number;
 	
+	@Size(min=0, max=30)
 	private String address;
 
 	public Integer getId() {
--- src/main/java/com/nikito/contacter/ContactController.java
+++ src/main/java/com/nikito/contacter/ContactController.java
@@ -1,8 +1,11 @@
 package com.nikito.contacter;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,7 +24,11 @@ public class ContactController {
 	}
 	
 	@PostMapping("/add")
-	public String add(@ModelAttribute Contact contact) {
+	public String add(@ModelAttribute @Valid Contact contact, BindingResult bindingResult) {
+		
+		if(bindingResult.hasErrors()) {
+			return "addContact";
+		}
 		contactRepository.save(contact);
 		return "addContact";
 	}
