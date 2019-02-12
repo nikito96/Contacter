5f086cfb9a1e235b95cf483e0cd194e6aef8eb05
nikito96 <nikinikolitsov9655@gmail.com>
2019-02-12 02:08:31 +0200

    some fixes

--- src/main/java/com/nikito/contacter/Contact.java
+++ src/main/java/com/nikito/contacter/Contact.java
@@ -4,6 +4,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -14,10 +15,10 @@ public class Contact {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Integer id;
 	
-	@NotNull
+	@NotBlank
 	private String name;
 	
-	@NotNull
+	@NotBlank
 	@Size(min=10, max=15)
 	private String number;
 	
